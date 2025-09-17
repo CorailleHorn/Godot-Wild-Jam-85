0ea5e32dca38fdd6eaf17ae76084c49032e2f31d
Coraille <jeansar504@gmail.com>
2025-09-17 12:23:39 +0200

    Planet resource
    Task #5 in progress

---
+++ Src/planet_resource.gd
@@ -0,0 +1,16 @@
+class_name planet_resource
+extends Resource
+
+
+@export var name: String = ""
+@export var image: Texture2D = null
+
+@export_group("Cost")
+@export var cost_caillou: int = 0
+@export var cost_gaz: int = 0
+@export var cost_flotte: int = 0
+
+@export_group("Effect")
+@export var effect_caillou: int = 0
+@export var effect_gaz: int = 0
+@export var effect_flotte: int = 0
